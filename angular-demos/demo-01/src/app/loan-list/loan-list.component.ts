ea153899cd53f8d6ad570d60137606c6a08a9bb9
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-07-26 12:39:06 +0530

    Render model data

--- angular-demos/demo-01/src/app/loan-list/loan-list.component.ts
+++ angular-demos/demo-01/src/app/loan-list/loan-list.component.ts
@@ -1,16 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loan-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
       List of Loans !
-    </p>
+      <div *ngFor="let l of loans">
+        <li>{{ l.customer }} {{ l.amount }}
+        </li>
+      </div>
   `,
   styles: ``
 })
 export class LoanListComponent {
-
+  loans = [
+    {"accNo":"HL12345", "customer":"Tushar Kapoor", "amount":1450000},
+    {"accNo":"HL12346", "customer":"Kapil Sharma", "amount":5500000}
+  ]
 }
